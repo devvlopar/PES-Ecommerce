8b6d163f0943a2dc98da0df9cc3b4c2b368e52e0
devvlopar <devangsigh34@gmail.com>
2023-12-28 15:48:45 +0530

    templates & buttons activate & header extension & model creation

--- buyer/views.py
+++ buyer/views.py
@@ -7,3 +7,21 @@ def home(request):
 
 def about_view(request):
     return render(request, 'about.html')
+
+def checkout_view(request):
+    return render(request, 'checkout.html')
+
+def faqs_view(request):
+    return render(request, 'faqs.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def header_view(request):
+    return render(request, 'header.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from buyer.views import home, about_view
+from buyer.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home"),
-    path('about/', about_view, name="about")
+    path('', home, name="index"),
+    path('about/', about_view, name="about"),
+    path('checkout/', checkout_view, name="checkout"),
+    path('contact/', contact_view, name="contact"),
+    path('faqs/', faqs_view, name="faqs"),
+    path('login/', login_view, name="login"),
+    path('register/', register_view, name="register"),
+    path('header/', header_view, name="header"),
+
+
+
 ]
