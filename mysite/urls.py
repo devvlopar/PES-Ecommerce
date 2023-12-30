5673e3be4aacd509e1124f5ce290c2f964d18d66
devvlopar <devangsigh34@gmail.com>
2023-12-30 11:00:41 +0530

    login & logout done

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path('register/', register_view, name="register"),
     path('header/', header_view, name="header"),
     path('otp/', otp_view, name="otp"),
+    path('logout/', logout_view, name="logout"),
+
 
 
 
